476269410fe1c5d45156d0435125979f75b8c4cd
Angie <lachristie.2021@scis.smu.edu.sg>
2022-10-16 16:46:49 +0800

    landing page and draft pages for the features

---
+++ unihelp/src/pages/settings.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import NavbarMain from '../components/NavbarMain';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
+
+function MyUni() {
+  const [isOpen, setIsOpen]  = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+  return (
+   <>
+    <Sidebar isOpen={isOpen} toggle={toggle} />
+    <NavbarMain toggle={toggle} />
+    <Footer />
+   </> 
+  )
+}
+
+export default MyUni;
