2452b372ed74ed9ba4a25e95e10ba8aee7043193
Angie <lachristie.2021@scis.smu.edu.sg>
2022-10-16 16:30:01 +0800

    landing page and draft pages for the features

--- unihelp/src/App.js
+++ unihelp/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import SigninPage from './pages/signin';
+import MainPage from './pages/main';
+import MyUni from './pages/myUni';
+import Forum from './pages/forum';
+import Settings from './pages/settings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={Home} exact />
+        <Route path='/signin' element={SigninPage} exact />
+        <Route path='/main-page' element={MainPage} exact />
+        <Route path='/forum' element={Forum} exact />
+        <Route path='/myUni' element={MyUni} exact />
+        <Route path='/settings' element={Settings} exact />
+      </Routes>
+    </Router>
   );
 }
 
