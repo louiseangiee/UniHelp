0d7bac54773f460a017a891d113c7ad18778496b
Phaandaa <90691224+Phaandaa@users.noreply.github.com>
2022-10-21 18:30:18 +0800

    Update firebaseConfig

--- unihelp/src/firebase/config.js
+++ unihelp/src/firebase/config.js
@@ -3,12 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAMPahuaywX_aiSrWRAgkqZX6qYHvLArE",
-  authDomain: "mymoney-3f1b7.firebaseapp.com",
-  projectId: "mymoney-3f1b7",
-  storageBucket: "mymoney-3f1b7.appspot.com",
-  messagingSenderId: "742069599459",
-  appId: "1:742069599459:web:70485bb44b3724bfa599d6"
+  apiKey: "AIzaSyDZEgmy-kvljNCiITyPeohpsVFOOGju9eo",
+  authDomain: "unihelp-e1a6d.firebaseapp.com",
+  databaseURL: "https://unihelp-e1a6d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "unihelp-e1a6d",
+  storageBucket: "unihelp-e1a6d.appspot.com",
+  messagingSenderId: "1022868403573",
+  appId: "1:1022868403573:web:095a08dedbc652112b4f3c",
+  measurementId: "G-V65SC8GHLZ"
 };
 
 // init firebase
