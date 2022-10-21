d450b655b296734fb7165486260bb27455654540
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2022-10-21 11:27:48 +0800

    updated /form card page pt 1

--- unihelp/src/pages/forum.js
+++ unihelp/src/pages/forum.js
@@ -6,6 +6,9 @@ import SearchBar from "../components/SearchBar";
 import tempfiletest from "../database/tempfiletest.json";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './stylesheets/forum.css';
+
+
 
 function Forum() {
   const [isOpen, setIsOpen]  = useState(false);
@@ -26,25 +29,29 @@ function Forum() {
 
     <div id= "schoolSelection">
       <h1 class ="header">Schools You Follow</h1>
-      <div id = "card" class ="row">
+      <div id = "cardRow" class ="row">
         <div class = "col-4">
-            <Card style= {{ width : '18rem', justifyContent: 'center'}}>
+          
+            <Card>
               <Card.Body>
-                <Card.Title class = "CardSchoolTitle" style={{}}> NUS </Card.Title>
+                <Card.Img variant='top' className="img-fluid " class = "logos" src = "logos/nus_logo.jpg"/>
+                <Card.Title id = "NUS title" class = "CardSchoolTitle" style={{textAlign: 'center'}}> NUS </Card.Title>
               </Card.Body>
             </Card>
         </div>
         <div class = "col-4">
-            <Card style= {{ width : '18rem'}}>
+            <Card>
               <Card.Body>
-                <Card.Title class = "CardSchoolTitle"> NTU </Card.Title>
+              <Card.Img variant='top' className="img-fluid " class = "logos" src = "logos/ntu_logo.jpg"/>
+                <Card.Title id = "NTU title" class = "CardSchoolTitle" style={{textAlign: 'center'}}> NTU </Card.Title>
               </Card.Body>
             </Card>
         </div>
         <div class = "col-4">
-            <Card style= {{ width : '18rem'}}>
+            <Card>
               <Card.Body>
-                <Card.Title class = "CardSchoolTitle"> SMU </Card.Title>
+              <Card.Img variant='top' className="img-fluid " class = "logos" src = "logos/smu_logo.jpg"/>
+                <Card.Title id = "SMU title" class = "CardSchoolTitle" style={{textAlign: 'center'}}> SMU </Card.Title>
               </Card.Body>
             </Card>
         </div>
