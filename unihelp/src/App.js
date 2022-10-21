b38241818de9c51cb37fce114e3305ee6a888cbd
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-10-21 16:02:15 +0800

    added sign up in app.js

--- unihelp/src/App.js
+++ unihelp/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
+import SignupPage from './pages/signup';
 import MainPage from './pages/main';
 import SubmitResults from './pages/submitResults';
 import Forum from './pages/forum';
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/signin' component={SigninPage} exact />
+        <Route path='/signup' component={SignupPage} exact />
         <Route path='/main-page' component={MainPage} exact />
         <Route path='/forum' component={Forum} exact />
         <Route path='/submit-results' component={SubmitResults} exact />
