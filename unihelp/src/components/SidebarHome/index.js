d2d2b77671867a651eee2376c5528d135f1beba3
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-10-25 18:26:25 +0800

    edited sidebar for home

---
+++ unihelp/src/components/SidebarHome/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap
+} from './SidebarHomeElements';
+
+const SidebarHome = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink
+            to='main-page'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            My Uni
+          </SidebarLink>
+          <SidebarLink
+            to='forum'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            Forum
+          </SidebarLink>
+          <SidebarLink
+            to='submit-results'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            Submit Results
+          </SidebarLink>
+
+          <SidebarLink
+            to='settings'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            Settings
+          </SidebarLink>
+        </SidebarMenu>
+        
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default SidebarHome;
