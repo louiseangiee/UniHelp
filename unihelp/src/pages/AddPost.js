387d3aae28b9e4786a0b7e0e50fca9b6f3f544cd
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2022-11-06 18:41:51 +0800

    addpost page

--- unihelp/src/pages/AddPost.js
+++ unihelp/src/pages/AddPost.js
@@ -19,7 +19,9 @@ import {
 } from "../components/SignUp/SignupElements";
 
 
+
 const AddPost = () =>{
+  
   const [school, setSchool] = useState("");
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
@@ -32,6 +34,52 @@ const AddPost = () =>{
       Title: ${title}
     `);
   };
+  return (
+    <>
+    <NavbarMain/>
+      <Container className=" my-3 p-5">
+        <FormWrap>
+          <FormContent>
+            <Form onSubmit={handleSubmit}>
+              <FormH1 className="my-4"> Submit a Forum Post </FormH1>
+              <FormLabel className="p-2" htmlFor="school"> School </FormLabel>
+              <Select className="mt-0"
+              required
+              id = "school"
+              onChange={(e) => setSchool(e.target.value)}
+              value = {school}>
+
+                <option selected value={null}>
+                  --Select an Option--
+                </option>
+                <option>NUS</option>
+                <option>NTU</option>
+                <option>SMU</option>
+                
+              </Select>
+
+              <FormLabel htmlFor="title"> Forum Title </FormLabel>
+              <FormInput type = "text" required id ="title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              />
+
+              <FormLabel htmlFor="content"> Forum Content </FormLabel>
+              <FormInput className = "h-100" type = "textarea" required id ="title"
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+              />
+
+              <FormButton className="my-5" type = "submit"> Post </FormButton>
+
+              
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+      <Footer/>
+    </>
+  )
   
 }
 
