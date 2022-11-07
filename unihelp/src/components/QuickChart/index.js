3083e650733399a9fdc0e1b962089dc425255cda
Yoza <yozafardhs.2021@scis.smu.edu.sg>
2022-11-07 17:11:08 +0800

    Update index.js

--- unihelp/src/components/QuickChart/index.js
+++ unihelp/src/components/QuickChart/index.js
@@ -1,12 +1,45 @@
 import React, { useState } from "react";
-const QuickChart = require('quickchart-js');
+function quickchart() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch("https://quickchart.io/chart?bkg=white&c={type:%27bar%27,data:{labels:[2012,2013,2014,2015,2016],datasets:[{label:%27Users%27,data:[120,60,50,180,120]}]}}")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 
-const myChart = new QuickChart();
-myChart.setConfig({
-  type: 'bar',
-  data: { labels: ['Hello world', 'Foo bar'], datasets: [{ label: 'Foo', data: [1, 2] }] },
-});
-
-console.log(myChart.getUrl());
-const url = await myChart.getShortUrl();
-console.log(url);
+export default quickchart;
