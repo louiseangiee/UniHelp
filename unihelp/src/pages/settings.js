4a9bdd2eca1f2ed53ad2e896b60dc8de3cc7f5b3
Davina Wijaya <phandavina@gmail.com>
2022-11-10 03:19:56 +0800

    settings backend working

--- unihelp/src/pages/settings.js
+++ unihelp/src/pages/settings.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavbarMain from '../components/NavbarMain';
 import SidebarHome from '../components/SidebarHome';
-import { useCollection } from '../hooks/useCollection'
+
 
 import Footer from '../components/Footer';
 import Button from 'react-bootstrap/Button';
@@ -10,6 +10,8 @@ import './stylesheets/submit-results.css';
 
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useLogout } from '../hooks/useLogout'
+import { useCollection } from '../hooks/useCollection'
+import { projectFirestore } from '../firebase/config'
 
 
 
@@ -17,13 +19,63 @@ function Settings() {
   const { logout, isPending } = useLogout()
   const { user } = useAuthContext()
   const [isOpen, setIsOpen] = useState(false);
-  const { documents, error } = useCollection('accountDetails')
+  const [isPendingData, setIsPendingData] = useState(false)
+  const [currentData, setCurrentData] = useState(null)
+  const [error, setError] = useState(false)
 
-  const [fullName, setFullName] = useState("");
-  const [studentType, setStudentType] = useState("");
-  const [HSQualification, setHSQualification] = useState("");
-  const [gradDate, setGradDate] = useState("");
-  const [DoB, setDoB] = useState("");
+  const [formFullName, setFormFullName] = useState("");
+  const [formStudentType, setFormStudentType] = useState("");
+  const [formStudentOrigin, setFormStudentOrigin] = useState("");
+  const [formHSQualification, setFormHSQualification] = useState("");
+  const [formEnglishTest, setFormEnglishTest] = useState("");
+  const [formGradDate, setFormGradDate] = useState("");
+  const [formDoB, setFormDoB] = useState("");
+
+
+
+  console.log(user.uid)
+
+  useEffect(() => {
+    setIsPendingData(true)
+    
+    const unsub = projectFirestore.collection('accountDetails').doc(user.uid).onSnapshot((doc) => {
+      if(doc.exists) {
+        setIsPendingData(false)
+        var data = doc.data()
+        setCurrentData(data)
+        setFormFullName(data.fullName)
+        setFormStudentType(data.studentType)
+        setFormStudentOrigin(data.studentOrigin)
+        setFormHSQualification(data.HSQualification)
+        setFormEnglishTest(data.englishTest)
+        setFormGradDate(data.gradDate)
+        setFormDoB(data.DoB)
+        
+      } else {
+        setIsPendingData(false)
+        setError('Could not fetch account details')
+      }
+    })
+
+    return () => unsub()
+
+  }, [user.uid])
+  
+  const saveChanges = (e) => {
+    e.preventDefault();
+    
+    console.log("help la")
+    projectFirestore.collection('accountDetails').doc(user.uid).update({
+      fullName: formFullName,
+      studentOrigin: formStudentOrigin,
+      HSQualification: formHSQualification,
+      englishTest: formEnglishTest,
+      gradDate: formGradDate,
+      DoB: formDoB
+    })
+  }
+
+  
   
 
   const toggle = () => {
@@ -31,79 +83,80 @@ function Settings() {
   }
   return (
     <>
-      <SidebarHome isOpen={isOpen} toggle={toggle} />
-      <NavbarMain toggle={toggle} />
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {currentData && (
+      <>
+        <SidebarHome isOpen={isOpen} toggle={toggle} />
+        <NavbarMain toggle={toggle} />
 
-      <div id="form" class="px-5 container col-xl-7">
-        <h1 class="header mx-0 px-0">Settings</h1>
+        <div id="form" class="px-5 container col-xl-7">
+          <h1 class="header mx-0 px-0">Settings</h1>
 
-        <Form>
-          <div className="row">
+          <Form>
+            <div className="row">
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" placeholder="Samuel No Surname" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicText">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" value={formFullName} onChange={(e) => setFormFullName(e.target.value)}/>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Label>E-Mail</Form.Label>
-              <Form.Control type="text" placeholder="samuel.2021@business.smu.edu.sg" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="select">
+                <Form.Label>I am a</Form.Label>
+                <Form.Control as="select" value={formStudentOrigin} onChange={(e) => setFormStudentOrigin(e.target.value)} >
+                  <option>Local Student</option>
+                  <option>International Student</option>
+                </Form.Control>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="select">
-              <Form.Label>Are you a</Form.Label>
-              <Form.Control as="select">
-                <option>Local Student</option>
-                <option>International Student</option>
-              </Form.Control>
-            </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="select">
+                <Form.Label>High School Qualification</Form.Label>
+                <Form.Control as="select" value={formHSQualification} onChange={(e) => setFormHSQualification(e.target.value)}>
+                  <option disabled selected>-- select an option --</option>
+                  <option>International Baccalaureate</option>
+                  <option>Cambridge A Level</option>
+                  <option>Indonesian Ujian Nasional</option>
+                  <option>SAT</option>
+                  <option>ACT</option>
+                </Form.Control>
+              </Form.Group>
 
-            <Form.Group className="mb-3 col-lg-6" controlId="select">
-              <Form.Label>High School Qualification</Form.Label>
-              <Form.Control as="select">
-                <option disabled selected>-- select an option --</option>
-                <option>International Baccalaureate</option>
-                <option>Cambridge A Level</option>
-                <option>Indonesian Ujian Nasional</option>
-                <option>SAT</option>
-                <option>ACT</option>
-              </Form.Control>
-            </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="select">
+                <Form.Label>English Proficiency Test</Form.Label>
+                <Form.Control as="select" value={formEnglishTest} onChange={(e) => setFormEnglishTest(e.target.value)}>
+                  <option disabled selected>-- select an option --</option>
+                  <option>IELTS</option>
+                  <option>TOEFL</option>
+                  <option>TOEIC</option>
+                </Form.Control>
+              </Form.Group>
 
-            <Form.Group className="mb-3 col-lg-6" controlId="select">
-              <Form.Label>English Proficiency Test</Form.Label>
-              <Form.Control as="select">
-                <option disabled selected>-- select an option --</option>
-                <option>IELTS</option>
-                <option>TOEFL</option>
-                <option>TOEIC</option>
-              </Form.Control>
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="DateOfBirth">
+                <Form.Label>Date of Birth</Form.Label>
+                <Form.Control type="date" value={formDoB} onChange={(e) => setFormDoB(e.target.value)}/>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="DateOfBirth">
-              <Form.Label>Date of Birth</Form.Label>
-              <Form.Control type="date" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="DateOfGrad">
+                <Form.Label>Expected Graduation Date</Form.Label>
+                <Form.Control type="date" value={formGradDate} onChange={(e) => setFormGradDate(e.target.value)}/>
+              </Form.Group>
+            </div>
 
-            <Form.Group className="mb-3" controlId="DateOfGrad">
-              <Form.Label>Expected Graduation Date</Form.Label>
-              <Form.Control type="date" />
-            </Form.Group>
-          </div>
+            <div className="row">
+              <Button className="mb-3" variant="primary" type="submit" onClick={saveChanges}>
+                Save Changes
+              </Button>
+            </div>
 
-          <div className="row">
-            <Button className="mb-3" variant="primary" type="submit">
-              Save Changes
-            </Button>
-          </div>
+            <div className="row">
+              {!isPending && <Button className="mb-3" variant="warning" onClick={logout}>Logout</Button>}
+              {isPending && <Button className="mb-3" variant="warning" disabled>Loging out...</Button>}
+            </div>
 
-          <div className="row">
-            {!isPending && <Button className="mb-3" variant="warning" onClick={logout}>Logout</Button>}
-            {isPending && <Button className="mb-3" variant="warning" disabled>Loging out...</Button>}
-          </div>
-
-        </Form>
-      </div>
+          </Form>
+        </div>
+      </>
+      )}
 
 
       <Footer />
