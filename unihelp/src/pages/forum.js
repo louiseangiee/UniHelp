aeda2a8ea594b88a53647f39c2c5ace47145c216
Davina Wijaya <phandavina@gmail.com>
2022-11-10 13:48:45 +0800

    forum post added to frontend but need styling

--- unihelp/src/pages/forum.js
+++ unihelp/src/pages/forum.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavbarMain from '../components/NavbarMain';
 import SidebarHome from '../components/SidebarHome';
 import Footer from '../components/Footer';
 import SearchBar from "../components/SearchBar";
+
+import { projectFirestore } from '../firebase/config'
 
 
 //These two files need to be changed
 import tempfiletest from "../database/SearchbartestFile.json";
 //how to import js file
 
-
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './stylesheets/forum.css';
 import Overlay from 'react-bootstrap/Overlay';
+import ForumCardList from '../components/ForumCardList';
 import FilterForums from '../components/SchoolFilterCards';
 import AddPostButton from '../components/AddPostButton';
 
-
-
 function Forum() {
+  console.log("masuk")
   const [isOpen, setIsOpen]  = useState(false);
-
   const [isActive, setIsActive] = useState(false);
   
   const toggle = () => {
@@ -30,13 +29,36 @@ function Forum() {
 
   }
   const handleClick = () => {
-    
     setIsActive(current => !current);
-
-    
   };
 
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+    const unsub = projectFirestore.collection('forumPost').onSnapshot((snapshot) => {
+      if(snapshot.empty){
+        setError('No posts to load')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }, (err) => {
+      setError(err.message)
+      setIsPending(false)
+    })
+    return () => unsub()
+  }, [])
+
   return (
+    
    <>
     <SidebarHome isOpen={isOpen} toggle={toggle} />
     <NavbarMain toggle={toggle} />
@@ -85,11 +107,15 @@ function Forum() {
       
     </div>
 
+    {error && <p className='error'>{error}</p>}
+    {isPending && <p className='loading'>Loading...</p>}
+
     <div id="forumBoxes">
-        
-        Filter By : <FilterForums></FilterForums>
-        
+      {data && <ForumCardList posts={data}/>}
     </div>
+        
+      {/* Filter By : <FilterForums></FilterForums> */}
+
     <AddPostButton></AddPostButton>
     
 
