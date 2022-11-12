97db15d5e8d8db29d9f02a468941f012f173cc9a
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-11-13 01:46:32 +0800

    tests

--- unihelp/src/components/Checklist/checklist.js
+++ unihelp/src/components/Checklist/checklist.js
@@ -9,7 +9,7 @@ import { useCollection } from '../../hooks/useCollection'
 
 const Checklist = () => {
     const { user } = useAuthContext();
-    const identifier = 'progress' + user.uid
+    const identifier = user.uid
     const [input, setInput] = useState("");
     const [todos, setTodos] = useState([]);
     const [editTodo, setEditTodo] = useState(null);
@@ -17,14 +17,12 @@ const Checklist = () => {
 
     useEffect(() => {
         setIsPendingData(true)
-
         const unsub = projectFirestore.collection('userProgress').doc(identifier).get().then(doc => {
             if (doc.exists) {
                 setIsPendingData(false)
                 var data = doc.data()
-                console.log(data)
                 setTodos(data.smu.checklist)
-                //console.log(data.smu.checklist)
+                
             }
         })
     }, [user.uid]);
