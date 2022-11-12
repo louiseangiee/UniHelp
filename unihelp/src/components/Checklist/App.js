923e0ac6b804d7dca761266ea6d0699b1d593f94
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-11-13 01:04:04 +0800

    test

--- unihelp/src/components/Checklist/App.js
+++ unihelp/src/components/Checklist/App.js
@@ -1,41 +1,54 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header"
 import Form from "./components/Form";
 import TodosList from "./components/TodosList";
 import "./App.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { projectFirestore } from '../../firebase/config'
+import { useCollection } from '../../hooks/useCollection'
 
 const App = () => {
-
-    const initialState = JSON.parse(localStorage.getItem("todos")) || [];
-    const[input, setInput] = useState("");
+    const { user } = useAuthContext();
+    const identifier = 'progress' + user.uid
+    const [input, setInput] = useState("");
     const [todos, setTodos] = useState([]);
     const [editTodo, setEditTodo] = useState(null);
+    const [isPendingData, setIsPendingData] = useState(false)
 
     useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
+        setIsPendingData(true)
 
-    
-    return(
-        <div className = "container">
+        const unsub = projectFirestore.collection('userProgress').doc(identifier).get().then(doc => {
+            if (doc.exists) {
+                setIsPendingData(false)
+                var data = doc.data()
+                console.log(data)
+                setTodos(data.smu.checklist)
+                //console.log(data.smu.checklist)
+            }
+        })
+    }, [user.uid]);
+
+    return (
+        <div className="container">
             <div className="app-wrapper">
                 <div>
                     <Header />
                 </div>
                 <div>
-                    <Form 
-                    input={input}
-                    setInput={setInput}
-                    todos={todos}
-                    setTodos={setTodos}
-                    editTodo={editTodo}
-                    setEditTodo={setEditTodo}
+                    <Form
+                        input={input}
+                        setInput={setInput}
+                        todos={todos}
+                        setTodos={setTodos}
+                        editTodo={editTodo}
+                        setEditTodo={setEditTodo}
                     />
                 </div>
                 <div>
-                    <TodosList todos={todos} 
-                        setTodos={setTodos} 
-                        setEditTodo={setEditTodo}/>
+                    <TodosList todos={todos}
+                        setTodos={setTodos}
+                        setEditTodo={setEditTodo} />
                 </div>
             </div>
         </div>);
