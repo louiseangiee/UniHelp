67b2077d4e6f9a5746a471d4b1a78dd887e3952b
Davina Wijaya <phandavina@gmail.com>
2022-11-12 13:31:47 +0800

    added schools and majors object

---
+++ unihelp/src/components/Majors.js
@@ -0,0 +1,18 @@
+export const schoolsAndMajors = {
+    '': [],
+    'Singapore Management University': [
+        'Accounting','Business', 'Economics', 
+        'Law', 'Information Systems', 'Computer Science',
+        'Computing & Law', 'Smart-City Management & Technology', 'Software Engineering',
+        'Politics, Law and Economics ', 'Political Science', 'Psychology', 'Sociology'
+    ],
+    'Nanyang Technological University': [
+        'Bachelor of Accountancy', 'Bachelor of Arts (Education)', 'Bachelor of Arts (Hons) in Chinese',
+        'Bachelor of Arts (Hons) in English', 'Bachelor of Arts (Hons) in History', 
+        'Bachelor of Arts (Hons) in Linguistics and Multilingual Studies', 
+        'Bachelor of Arts (Hons) in Philosophy', 'Bachelor of Business',
+        'Bachelor of Communication Studies'
+
+    ],
+    'National University of Singapore': [ 'Computer Science']
+}
