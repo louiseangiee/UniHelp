d7b1be52a628a8bba54e03bcf0c022980ae79df3
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-11-12 15:30:21 +0800

    Majors

--- unihelp/src/components/Majors.js
+++ unihelp/src/components/Majors.js
@@ -14,19 +14,18 @@ export const schoolsAndMajors = {
         'Communication Studies', 'Aerospace Engineering', 'Bioengineering',
         'Chemical and Biomolecular Engineering', 'Civil Engineering', 'Computer Engineering',
         'Computer Science', 'Engineering in Electrical and Electronic Engineering(EEE)', 'Environmental Engineering',
-        'Information Engineering and Media(IEM)', 'Materials Engineering', 'Mechanical Engineering',
-
-
+        'Information Engineering and Media(IEM)', 'Materials Engineering', 'Mechanical Engineering', 'Economics', 
+        'Economics and Data Science','Economics', 'Social Science','Social Science in Public Policy and Global Affairs'
     ],
     'National University of Singapore': [ 
         'Data Science and Economics', 'Food Science and Technology',
         'Humanities and Sciences', 'Pharmaceutical Science', 'Philosophy, Politics, and Economics',
         'Business Administration (Accountancy)', 'Business Administration', 'Real Estate',
-        'Business Analytics', 'Computer Science', 'Information Security', 'Information Systems'
+        'Business Analytics', 'Computer Science', 'Information Security', 'Information Systems','Computer Engineering',
+        'Dentistry','Architecture','Engineering','Industrial Design','Landscape Architecture','Medicine','Nursing','Pharmacy','Music'
     ]
 }
 
 //how to accomodate double degree :o
 //https://www.ntu.edu.sg/admissions/undergraduate-programmes?listingKeyword=&disciplines=all&programmelevels=all&programmetypes=all&page=7
 
-//https://www.nus.edu.sg/oam/undergraduate-programmes 
