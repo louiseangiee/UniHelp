22bac059f15cc33b8a73fbc5f7c2f15c5a064838
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-11-12 21:57:04 +0800

    toasts

--- unihelp/src/pages/main.js
+++ unihelp/src/pages/main.js
@@ -1,26 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import NavbarMain from '../components/NavbarMain';
 import SidebarHome from '../components/SidebarHome';
 import Footer from '../components/Footer';
 // import Services from '../components/Services';
 // import SchoolTabs from '../components/SchoolTabs';
-import {IntroMyUni, ChooseUni} from '../components/ChooseUni';
+import { IntroMyUni, ChooseUni } from '../components/ChooseUni';
+import toast, { Toaster } from 'react-hot-toast';
+import { ReactSession } from 'react-client-session';
+ReactSession.setStoreType("localStorage");
+
 
 function MainPage() {
-  const [isOpen, setIsOpen]  = useState(false);
 
+  const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
     setIsOpen(!isOpen);
   }
+
+  useEffect(() => { 
+    while(ReactSession.get("isLoggedin")){
+    toast.success('You have successfully signed in') 
+    ReactSession.set("isLoggedin", false)
+    }
+  }, console.log('error'));
+
+
+
   return (
-   <>
-    <SidebarHome isOpen={isOpen} toggle={toggle} />
-    <NavbarMain toggle={toggle} />
-    {/* <SchoolTabs /> */}
-    {/* <IntroMyUni /> */}
-    <ChooseUni />
-    <Footer />
-   </> 
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          // Default options for specific types
+          success: {
+            duration: 2000,
+          },
+          style: {
+            border: '2px solid #5271ff',
+            padding: '16px',
+          },
+        }}
+      />
+      <SidebarHome isOpen={isOpen} toggle={toggle} />
+      <NavbarMain toggle={toggle} />
+      {/* <SchoolTabs /> */}
+      {/* <IntroMyUni /> */}
+      <ChooseUni />
+      <Footer />
+    </>
   )
 }
 
