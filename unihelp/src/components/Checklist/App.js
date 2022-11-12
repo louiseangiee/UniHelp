103cc473dacd019fdd277cc6e139bc3c38ea0003
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2022-11-12 23:51:18 +0800

    checklist draft

--- unihelp/src/components/Checklist/App.js
+++ unihelp/src/components/Checklist/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./components/Header"
 import Form from "./components/Form";
 import TodosList from "./components/TodosList";
@@ -6,8 +6,16 @@ import "./App.css";
 
 const App = () => {
 
+    const initialState = JSON.parse(localStorage.getItem("todos")) || [];
     const[input, setInput] = useState("");
     const [todos, setTodos] = useState([]);
+    const [editTodo, setEditTodo] = useState(null);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    
     return(
         <div className = "container">
             <div className="app-wrapper">
@@ -20,10 +28,14 @@ const App = () => {
                     setInput={setInput}
                     todos={todos}
                     setTodos={setTodos}
+                    editTodo={editTodo}
+                    setEditTodo={setEditTodo}
                     />
                 </div>
                 <div>
-                    <TodosList todos={todos} setTodos={setTodos}/>
+                    <TodosList todos={todos} 
+                        setTodos={setTodos} 
+                        setEditTodo={setEditTodo}/>
                 </div>
             </div>
         </div>);
