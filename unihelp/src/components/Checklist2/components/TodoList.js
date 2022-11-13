2edfb2602f7f40498334cd16ab60c172b3a52867
DENNIS HARDIANTO <dennish.2021@scis.smu.edu.sg>
2022-11-13 23:39:32 +0800

    final

--- unihelp/src/components/Checklist2/components/TodoList.js
+++ unihelp/src/components/Checklist2/components/TodoList.js
@@ -8,56 +8,56 @@ import { useAuthContext } from "../../../hooks/useAuthContext";
 import { useFirestore } from "../../../hooks/useFirestore";
 
 export default function TodoList({ todo, todos, uni }) {
-  const { user } =  useAuthContext()
+  const { user } = useAuthContext()
   const { updateDocument, response } = useFirestore('userProgress')
   const [newTitle, setNewTitle] = useState(todo.title)
   const [newDeadline, setNewDeadline] = useState(todo.deadline)
 
   const toggleComplete = async (todo) => {
-    for(var i = 0; i < todos.length; i++){
-      if(todos[i] === todo){
-        if(todos[i].done === false){
-          todos[i] = {...todo, done: true}
+    for (var i = 0; i < todos.length; i++) {
+      if (todos[i] === todo) {
+        if (todos[i].done === false) {
+          todos[i] = { ...todo, done: true }
         } else {
-          todos[i] = {...todo, done: false}
-          
+          todos[i] = { ...todo, done: false }
+
         }
       }
     }
 
     console.log(todos)
-    if(uni === 'nus'){
-      await updateDocument(`progress${user.uid}`,{
+    if (uni === 'nus') {
+      await updateDocument(`progress${user.uid}`, {
         nus: todos
       })
-    } else if (uni === 'ntu'){
-      await updateDocument(`progress${user.uid}`,{
+    } else if (uni === 'ntu') {
+      await updateDocument(`progress${user.uid}`, {
         ntu: todos
       })
-    } else if (uni === 'smu'){
-      await updateDocument(`progress${user.uid}`,{
+    } else if (uni === 'smu') {
+      await updateDocument(`progress${user.uid}`, {
         smu: todos
       })
     }
   };
 
   const handleDelete = async (todo) => {
-    for(var i = 0; i < todos.length; i++) {
+    for (var i = 0; i < todos.length; i++) {
       if (todos[i] == todo) {
-          todos.splice(i, 1);
+        todos.splice(i, 1);
       }
     }
     console.log(todos)
-    if(uni === 'nus'){
-      await updateDocument(`progress${user.uid}`,{
+    if (uni === 'nus') {
+      await updateDocument(`progress${user.uid}`, {
         nus: todos
       })
-    } else if (uni === 'ntu'){
-      await updateDocument(`progress${user.uid}`,{
+    } else if (uni === 'ntu') {
+      await updateDocument(`progress${user.uid}`, {
         ntu: todos
       })
-    } else if (uni === 'smu'){
-      await updateDocument(`progress${user.uid}`,{
+    } else if (uni === 'smu') {
+      await updateDocument(`progress${user.uid}`, {
         smu: todos
       })
     }
@@ -71,27 +71,27 @@ export default function TodoList({ todo, todos, uni }) {
           <button
             className="button-complete"
             onClick={() => toggleComplete(todo)}>
-          <img src= {"logos/complete.png"} className="logo-todo"></img>
+            <img src={todo.done ? "logos/check-box.png" : "logos/square.png"} className="logo-todo"></img>
           </button>
         </div>
 
-        <div className="col d-flex justify-content-center align-items-center"> 
+        <div className="col d-flex justify-content-center align-items-center">
           <p className="list">{todo.name}</p>
         </div>
-        
+
         {/* <div className="col-5"> 
           <p className="list">{todo.deadline}</p>
         </div> */}
 
-        <div className="col-1 d-flex justify-content-end"> 
-          <button className="button-delete"  onClick={() => handleDelete(todo)}>
-            <img src= {"logos/delete.png"} className="logo-todo"></img>
+        <div className="col-1 d-flex justify-content-end">
+          <button className="button-delete" onClick={() => handleDelete(todo)}>
+            <img src={"logos/delete.png"} className="logo-todo"></img>
           </button>
         </div>
 
       </div>
-      </>
-      
-      
+    </>
+
+
   );
 }
