970d73d702a060d027547aefe7a76952af30db88
Angie <lachristie.2021@scis.smu.edu.sg>
2022-11-13 14:31:32 +0800

    formatting sort button group

--- unihelp/src/pages/forum.js
+++ unihelp/src/pages/forum.js
@@ -1,59 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { useCollection } from '../hooks/useCollection'
-import { useAuthContext } from '../hooks/useAuthContext'
+import React, { useState, useEffect } from "react";
+import { useCollection } from "../hooks/useCollection";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //components
-import NavbarMain from '../components/NavbarMain';
-import SidebarHome from '../components/SidebarHome';
-import Footer from '../components/Footer';
+import NavbarMain from "../components/NavbarMain";
+import SidebarHome from "../components/SidebarHome";
+import Footer from "../components/Footer";
 import SearchBar from "../components/SearchBar";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import './stylesheets/forum.css';
-import Overlay from 'react-bootstrap/Overlay';
-import ForumCardList from '../components/ForumCardList';
-import FilterForums from '../components/SchoolFilterCards/index';
-import AddPostButton from '../components/AddPostButton';
-import toast, { Toaster } from 'react-hot-toast';
-import { ReactSession } from 'react-client-session';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./stylesheets/forum.css";
+import Overlay from "react-bootstrap/Overlay";
+import ForumCardList from "../components/ForumCardList";
+import FilterForums from "../components/SchoolFilterCards/index";
+import AddPostButton from "../components/AddPostButton";
+import toast, { Toaster } from "react-hot-toast";
+import { ReactSession } from "react-client-session";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ToggleButton from "react-bootstrap/ToggleButton";
 ReactSession.setStoreType("localStorage");
 
 function Forum() {
-
   useEffect(() => {
     while (ReactSession.get("addedPost")) {
-      toast.success('You have added a Post')
-      ReactSession.set("addedPost", false)
+      toast.success("You have added a Post");
+      ReactSession.set("addedPost", false);
     }
-  }, console.log('error'));
-
+  }, console.log("error"));
 
   function setNew() {
-    setNewToOld(true)
-    setOldToNew(false)
-    setVotes(false)
-    setonLoad(false)
+    setNewToOld(true);
+    setOldToNew(false);
+    setVotes(false);
+    setonLoad(false);
   }
 
-
   function setOld() {
-    setNewToOld(false)
-    setOldToNew(true)
-    setVotes(false)  
-    setonLoad(false)
-
+    setNewToOld(false);
+    setOldToNew(true);
+    setVotes(false);
+    setonLoad(false);
   }
 
   function setVote() {
-    setNewToOld(false)
-    setOldToNew(false)
-    setVotes(true)  
-    setonLoad(false)
-
+    setNewToOld(false);
+    setOldToNew(false);
+    setVotes(true);
+    setonLoad(false);
   }
-
-
 
   const [isOpen, setIsOpen] = useState(false);
   const [isActive, setIsActive] = useState(false);
@@ -62,39 +56,40 @@ function Forum() {
   const [highvotes, setVotes] = useState(false);
   const [onLoad, setonLoad] = useState(true);
 
-
-
   const toggle = () => {
     setIsOpen(!isOpen);
-  }
+  };
   const handleClick = () => {
-    setIsActive(current => !current);
+    setIsActive((current) => !current);
   };
 
-  const { user } = useAuthContext()
-  const { documents, error } = useCollection('forumPost')
-  const [filter, setFilter] = useState('all')
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection("forumPost");
+  const [filter, setFilter] = useState("all");
 
   const changeFilter = (newFilter) => {
-    setFilter(newFilter)
-  }
+    setFilter(newFilter);
+  };
 
-  const posts = documents ? documents.filter(document => {
-    switch (filter) {
-      case 'all':
-        return true
-      case 'NUS':
-      case 'NTU':
-      case 'SMU':
-        console.log(document.school, filter)
-        return document.school === filter
-      default:
-        return true
-    }
-  }) : null
+  const posts = documents
+    ? documents.filter((document) => {
+        switch (filter) {
+          case "all":
+            return true;
+          case "NUS":
+          case "NTU":
+          case "SMU":
+            console.log(document.school, filter);
+            return document.school === filter;
+          default:
+            return true;
+        }
+      })
+    : null;
+
+  const ButtonGroupStyle = {};
 
   return (
-
     <>
       <Toaster
         position="top-right"
@@ -104,8 +99,8 @@ function Forum() {
             duration: 2000,
           },
           style: {
-            border: '2px solid #5271ff',
-            padding: '16px',
+            border: "2px solid #5271ff",
+            padding: "16px",
           },
         }}
       />
@@ -113,10 +108,10 @@ function Forum() {
       <NavbarMain toggle={toggle} />
 
       {/* Make Searchbar stick to navbar */}
-      {error && <p className='error'>{error}</p>}
+      {error && <p className="error">{error}</p>}
 
-      <div id = "searchBar">
-      <SearchBar placeholder="Search" data={posts} />
+      <div id="searchBar">
+        <SearchBar placeholder="Search" data={posts} />
       </div>
 
       <div id="schoolSelection">
@@ -124,28 +119,67 @@ function Forum() {
         {documents && <FilterForums changeFilter={changeFilter} />}
       </div>
 
-      <ButtonGroup aria-label="Basic example">
-        <Button onClick={setNew}>Newest</Button>
-        <Button onClick={setOld}>Oldest</Button>
-        <Button onClick={setVote}>Vote</Button>
-      </ButtonGroup>
+      <div className="container px-0 pb-3 d-flex align-items-center justify-content-center">
+        {/* <span>Sort By:</span> */}
+        <ButtonGroup>
+          <ToggleButton
+            type="radio"
+            variant={"outline-primary"}
+            name="radio"
+            onClick={setNew}
+            value={newToOld}
+            checked={newToOld}
+          >
+            Newest
+          </ToggleButton>
+          <ToggleButton
+            type="radio"
+            variant={"outline-primary"}
+            name="radio"
+            onClick={setOld}
+            value={oldToNew}
+            checked={oldToNew}
+          >
+            Oldest
+          </ToggleButton><ToggleButton
+            type="radio"
+            variant={"outline-primary"}
+            name="radio"
+            onClick={setVote}
+            value={!highvotes}
+            checked={highvotes}
+          >
+            Most Likes
+          </ToggleButton>
+        </ButtonGroup>
+      </div>
 
       <div id="forumBoxes">
-        {posts && onLoad && <ForumCardList posts={posts.sort((a, b) => b.votes - a.votes)}/>}
-        {posts && newToOld && <ForumCardList posts={posts.sort((a, b) => b.createdAt - a.createdAt)}  />}
-        {posts && oldToNew && <ForumCardList posts={posts.sort((a, b) => a.createdAt - b.createdAt)}  />}
-        {posts && highvotes && <ForumCardList posts={posts.sort((a, b) => b.votes - a.votes)} />}
+        {posts && onLoad && (
+          <ForumCardList posts={posts.sort((a, b) => b.votes - a.votes)} />
+        )}
+        {posts && newToOld && (
+          <ForumCardList
+            posts={posts.sort((a, b) => b.createdAt - a.createdAt)}
+          />
+        )}
+        {posts && oldToNew && (
+          <ForumCardList
+            posts={posts.sort((a, b) => a.createdAt - b.createdAt)}
+          />
+        )}
+        {posts && highvotes && (
+          <ForumCardList posts={posts.sort((a, b) => b.votes - a.votes)} />
+        )}
       </div>
 
       {/* Filter By : <FilterForums></FilterForums> */}
 
       <AddPostButton></AddPostButton>
 
-
       <Footer />
     </>
-  )
+  );
 }
 
 export default Forum;
-
